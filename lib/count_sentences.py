06cc07fe9d58ba2c80c44cc04b899acd10affef0
Mulindi123 <medrine.mulindi@gmail.com>
2023-09-24 18:03:19 +0300

    Attempted assignment

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,40 @@
 #!/usr/bin/env python3
 
 class MyString:
-  pass
+  def __init__(self, value= None):
+    self.value = value
+  
+  def get_value(self):
+    return self._value 
+  
+  def set_value(self, value):
+    if isinstance(value, str):
+      self._value = value
+    
+    else:
+      print("The value must be a string.")
+  
+  value = property(get_value, set_value)
+
+  def is_sentence(self):
+    if self._value and self._value.endswith("."):
+      return True
+    else:
+      return False
+    
+  def is_question(self):
+    if self._value and self._value.endswith("?"):
+      return True
+    else:
+      return False
+    
+  def is_exclamation(self):
+    if self._value and self._value.endswith("!"):
+      return True
+    else:
+      return False
+
+my_string = MyString("I love singing")
+my_string.value = "Do I love singing!"
+print(my_string.is_exclamation())
+# print(my_string.value)
